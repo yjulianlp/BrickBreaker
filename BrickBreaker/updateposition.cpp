26130163d194614fe3e0ddf94cbe3e96a88856a3
Yujin <146316031+yjulianlp@users.noreply.github.com>
2024-10-10 03:38:33 -0600

    bounce ball off the sides of the screen

--- BrickBreaker/updateposition.cpp
+++ BrickBreaker/updateposition.cpp
@@ -4,10 +4,18 @@
 
 void updateBall(Ball& ball, float time) {
 	std::vector<int> pos = ball.getCenter();
-	std::cout << "angle: " << ball.getAngle();
+	std::cout << "current angle: " << ball.getAngle() << "\n";
 	ball.setXVelocity(ball.getSpeed() * cos(ball.getAngle()));
 	ball.setYVelocity(ball.getSpeed() * sin(ball.getAngle()));
 	std::vector<int> velo = ball.getVelocity();
 
 	ball.movePosition(time);
+
+	pos = ball.getCenter();
+	if (pos[0]-BALL_RADIUS <= 0 || pos[0]+BALL_RADIUS >= SCREEN_WIDTH) {
+		ball.setAngle(PI - ball.getAngle());
+	}
+	if (pos[1] - BALL_RADIUS < 0 || pos[1] + BALL_RADIUS > SCREEN_HEIGHT) {
+		ball.setAngle(-ball.getAngle());
+	}
 }
